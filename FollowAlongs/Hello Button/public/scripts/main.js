6e8131254a5acc873033539c92bdb3c5ccb20ace
Trey <srkline3@gmail.com>
2021-09-07 10:54:22 -0400

    jabra scp

--- FollowAlongs/Hello Button/public/scripts/main.js
+++ FollowAlongs/Hello Button/public/scripts/main.js
@@ -1,5 +1,24 @@
+const STARTING_VAL = 0
+var counter = STARTING_VAL
+
 function main(){
-    console.log("Ready")
+    console.log("Ready");
+    document.querySelector("#decButton").onclick = (event) => {
+        counter = counter - 1;
+        updateView();
+    }
+    document.querySelector("#incButton").onclick = (event) => {
+        counter = counter + 1;
+        updateView();
+    }
+    document.querySelector("#rButton").onclick = (event) => {
+        counter = STARTING_VAL;
+        updateView();
+    }
+}
+
+function updateView() {
+    document.querySelector("#count").innerHTML = `Count = ${counter}`;
 }
 
 main()
